a7547048e4510c0b28181ee2afdf234e5401ecd6
Victor Jaber <vjaber@artemistech.com.br>
2026-02-06 20:58:03 +0000

    Add offices and lawyers to the database when seeding data
    Introduce `seedData` function to populate `enderecos`, `contatos`, `advogados`, `escritorios`, `reclamantes`, `processos`, and associated junction tables in the database.

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
 import logger from "./logger";
-import { seedUsers } from "./seed";
+import { seedUsers, seedData } from "./seed";
 
 const app = express();
 const httpServer = createServer(app);
@@ -44,6 +44,7 @@ app.use((req, res, next) => {
 
 (async () => {
   await seedUsers();
+  await seedData();
   await registerRoutes(httpServer, app);
 
   app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
